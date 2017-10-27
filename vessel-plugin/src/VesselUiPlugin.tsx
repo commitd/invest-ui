352149263095e513e4d50cd891afefc552312ccf
Chris Flatley <chris@committed.io>
2017-10-27 16:18:46 +0200

    Correct iframe code and pass api via context

--- vessel-plugin/src/VesselUiPlugin.tsx
+++ vessel-plugin/src/VesselUiPlugin.tsx
@@ -1,26 +1,38 @@
 import * as React from 'react'
+import * as PropTypes from 'prop-types'
 import { Connection } from 'vessel-rpc'
 import { createApolloRpcClient } from 'vessel-graphql'
 import { ApolloProvider, ApolloClient } from 'react-apollo'
 import { Handler } from 'vessel-rpc'
 
+import { VesselPluginApi } from './VesselPluginApi'
 import { PluginLifecycle } from 'vessel-common'
 
 export interface Props {
     handler: Handler<{} & PluginLifecycle>
 }
 
+export interface Context {
+    pluginApi: VesselPluginApi
+}
+
 export class VesselUiPlugin extends React.Component<Props> {
+    static childContextTypes = {
+        pluginApi: PropTypes.instanceOf(VesselPluginApi)
+    }
+
     client: ApolloClient
     connection: Connection<{}>
+    pluginApi: VesselPluginApi
 
     constructor(props: Props) {
         super(props)
 
         const handler = this.props.handler
-
-        if (!this.isInIFrame) {
+        console.log('hello')
+        if (!this.isInIFrame()) {
             console.log('Running standalone')
+            // TODO: We need to discard the vesselUI stuff when passed to this. Messy
             this.client = new ApolloClient()
         } else {
             console.log('Running inside vessel')
@@ -31,6 +43,13 @@ export class VesselUiPlugin extends React.Component<Props> {
             })
         }
 
+        this.pluginApi = new VesselPluginApi(this.client, this.connection)
+    }
+
+    getChildContext(): Context {
+        return {
+            pluginApi: this.pluginApi
+        }
     }
 
     render() {
