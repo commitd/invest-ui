4a253861a6a15b454a1a93151d3fb3507853ef79
Chris Flatley <chris@committed.io>
2017-10-30 22:02:01 +0000

    Correct todos

--- vessel-framework/src/handlers/GlobalHandler.ts
+++ vessel-framework/src/handlers/GlobalHandler.ts
@@ -16,7 +16,8 @@ export function newGlobalHandler(client: ApolloClient, sessionProvider: () => st
     const h = new GlobalHandler()
     h.graphql = createGraphQLHandlerForClient(client)
     h.fetch = function (input: RequestInfo, init?: RequestInit): Promise<Response> {
-        // TODO: Add the Auth here
+        // TODO: We should perhaps clone the init, before we modify to avoid leaking session back into the caller
+        // TODO: Check if the input is for our server or another. Otherwise we are sending our session to other people
 
         if (init == null) {
             init = {}
