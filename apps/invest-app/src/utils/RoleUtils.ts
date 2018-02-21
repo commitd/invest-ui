b45a4d8ed36eda447e7e8c23fe5508321864fb53
Chris Flatley <chris@committed.io>
2018-02-21 10:23:09 +0000

    DIsable plugin filtering (until auth state is available in UI)

--- apps/invest-app/src/utils/RoleUtils.ts
+++ apps/invest-app/src/utils/RoleUtils.ts
@@ -2,14 +2,15 @@ import { UiPlugin } from 'invest-types'
 import { State as AuthState } from '../redux/reducers/auth'
 
 export function canUserSeePlugin(auth: AuthState, plugin: UiPlugin) {
-    const userRoles = auth.roles
-    const pluginRoles = plugin.roles
+    return true
+    // const userRoles = auth.roles
+    // const pluginRoles = plugin.roles
 
-    if (pluginRoles) {
-        // Does the user have all of the plugins roles?
-        return pluginRoles.every(r => userRoles.includes(r))
-    } else {
-        // If the plugin has no roles, then anyone can use it 
-        return true
-    }
+    // if (pluginRoles) {
+    //     // Does the user have all of the plugins roles?
+    //     return pluginRoles.every(r => userRoles.includes(r))
+    // } else {
+    //     // If the plugin has no roles, then anyone can use it 
+    //     return true
+    // }
 }
