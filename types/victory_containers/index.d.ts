a3fae4832acabe01730186acf3de25dc36b1dc0e
Chris Flatley <chris@committed.io>
2018-03-06 10:36:17 +0000

    Augment types for victory

---
+++ types/victory_containers/index.d.ts
@@ -0,0 +1,17 @@
+
+// This is very basic, just enough for us in TimelineChart
+// it would need amending in other use cases.
+// This is augmenting the types from @types/victory
+
+declare module "victory" {
+    export type BrushDomain = { x?: [Date, Date], y?: [Date, Date] }
+
+    export type VictoryBrushContainerProps = {
+        brushDimension: string,
+        brushDomain?: BrushDomain,
+        onBrushDomainChange(domain: BrushDomain): void,
+        responsive?: boolean
+    } 
+ 
+    export class VictoryBrushContainer extends React.Component<VictoryBrushContainerProps, {}> { }
+}
