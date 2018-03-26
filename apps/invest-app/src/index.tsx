5095903d864846427c4389b8e321efd378e742c5
Chris Flatley <chris@committed.io>
2018-03-26 13:27:56 +0100

    Remove unused import

--- apps/invest-app/src/index.tsx
+++ apps/invest-app/src/index.tsx
@@ -2,7 +2,7 @@ import * as React from 'react'
 import * as ReactDOM from 'react-dom'
 import { Provider } from 'react-redux'
 import { Router } from 'react-router-dom'
-import registerServiceWorker from './registerServiceWorker'
+// import registerServiceWorker from './registerServiceWorker'
 import './index.css'
 import 'semantic-ui-offline/semantic.min.css'
 
